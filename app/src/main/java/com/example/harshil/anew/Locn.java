34c7843ee83908be8f81bd6ba66b857e89362205
Vamsidhar Muthireddy <vamsi.muthireddy@gmail.com>
2016-11-09 17:36:55 +0530

    Update Locn.java
    Added comments

--- app/src/main/java/com/example/harshil/anew/Locn.java
+++ app/src/main/java/com/example/harshil/anew/Locn.java
@@ -4,6 +4,10 @@ package com.example.harshil.anew;
  * Created by Harshil on 7/2/2016.
  */
 public class Locn {
+    /**
+    *This class sets and gets latitude and longitude of the location
+    *
+    */
     public double longtd;
     public double latd;
 
